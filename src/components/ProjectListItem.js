f841c1d0fae759af613252142c4ee8acad71ea7b
bryttanie <bhouse@pdx.edu>
2020-12-14 16:36:52 -0800

    added list item component pt 2

---
+++ src/components/ProjectListItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, 
+    NavLink,
+    useRouteMatch 
+} from "react-router-dom"; 
+
+const ProjectListItem = (props) => {
+
+    let { path, url } = useRouteMatch();
+
+    return(
+        <li className="project-list-item">
+            <button className="project-button">
+                <NavLink 
+                    className="project-link"
+                    to={`${url}/${props.path}`}
+                >{props.title}
+                </NavLink>
+            </button>
+        </li>
+    )
+}
+
+export default ProjectListItem;

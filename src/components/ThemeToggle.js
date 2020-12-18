2ce99daac9a5157f17732ec883da7c10fa752a24
bryttanie <bhouse@pdx.edu>
2020-12-17 22:40:26 -0800

    fixing load resource error

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -2,14 +2,14 @@ import React, {useState} from 'react';
 import '../styles/components/ThemeToggle.css';
 
 const ThemeToggle = () => {
-    const [theme, setTheme] = useState("day");
+    const [theme, setTheme] = useState("light");
 
     const handleSwitchTheme = () => {
         let element = document.body;
         element.classList.toggle("day-mode");
-        if(theme === "day")
-            setTheme("night");
-        else setTheme("day");
+        if(theme === "light")
+            setTheme("dark");
+        else setTheme("light");
     }
 
     return (
